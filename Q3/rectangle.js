9aea970ee34ce121944c5825fb88639ef496c6e1
dhrumit <dhrumitbarot1@gmail.com>
2023-01-24 03:59:08 -0600

    rectangle.js added for Q3 calculation.

---
+++ Q3/rectangle.js
@@ -0,0 +1,27 @@
+
+
+export   function calculateRectangleArea(length, width) {
+    let area = lenght * width;
+    return area;
+}
+console.log(area)
+
+
+
+
+
+
+
+
+//     function calculateRectangleArea(length, width) {
+//         return length * width;
+//     }
+   
+    
+//     console.log('The area of your rectangle is ' + calculateRectangleArea(length, width));   
+
+//     function calculateRectangleArea(length, width) {
+//         let area = lenght * width;
+//         return area;
+//     }
+//     console.log(area)
