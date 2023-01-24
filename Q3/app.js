ec754b133eda73cf12e5112a6a8b37d540b8efa8
dhrumit <dhrumitbarot1@gmail.com>
2023-01-24 03:49:49 -0600

    apps.js created for calculates the area of a rectangle in Q3.

---
+++ Q3/app.js
@@ -0,0 +1,6 @@
+// function from the rectangle.js modul
+import { calculateRectangleArea } from "./rectangle.js";
+
+// calling the function by putting values and hold it into a variable
+let area = calculateRectangleArea(5, 6);
+console.log(area);
